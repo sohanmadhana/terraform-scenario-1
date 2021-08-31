4f32b85f5f169252ac0452019c66e094226348e2
Sohan Madhana <54635473+sohanmadhana@users.noreply.github.com>
2021-08-31 22:18:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,6 +3,6 @@ provider "tls" {
 }
 provider "aws" {
     region = "us-west-1"
-    shared_credentials_file = "F:\\terraform-tasks\\.aws\\credentials"
+    shared_credentials_file = <provide-location-of-the-aws-credentials-file>
   
 }
